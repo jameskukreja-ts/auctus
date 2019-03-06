5bc1a20ff89c5da7f143077e8cf6e7c8ebea20aa
James <james.kukreja@twinspark.co>
2019-03-06 20:01:41 +0530

    update promise

--- auctus-short-codes/promise.php
+++ auctus-short-codes/promise.php
@@ -1,22 +1,22 @@
 <?php 
-// echo '<pre>';
-// print_r($attrs);
-// die();
+echo '<pre>';
+print_r($attrs);
+die();
 ?>
 <div id="Promise" class="coustomer container">
 <div class="multi_logos">
    <h1>OUR CUSTOMER PROMISE</h1>
    <div class="col-md-6">
-      <img src="<?php echo get_stylesheet_directory_uri()?>/assets/images/costumer_img.jpg" class="img-responsive" style="width:100%;">
+      <img src="<?php echo $attrs['img'];?>" class="img-responsive" style="width:100%;">
    </div>
    <div class="col-md-6">
 <div class="left_containt">
-<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </h4>
+<h4><?php echo $attrs['boldtext'];?> </h4>
 </div>
 
 <div class="right_contant">
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
-<br><img src="<?php echo get_stylesheet_directory_uri()?>/assets/images/signature.png">
+<?php echo $attrs['plaintext'];?>
+<br><img src="<?php echo $attrs['signatureimg'];?>">
 
 
 </div>
