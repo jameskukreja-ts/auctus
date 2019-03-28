529ccf68e6353ff22edf7ab564b042a312d7afd6
James <james.kukreja@twinspark.co>
2019-03-28 14:23:48 +0530

    Fixed heading nesting issues

--- auctus-short-codes/hero.php
+++ auctus-short-codes/hero.php
@@ -25,7 +25,7 @@ if(isset($attrs['buttontext']) && !empty($attrs['buttontext'])){
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Modal Header</h4>
+          <h3 class="modal-title">Modal Header</h3>
         </div>
         <div class="modal-body">
           <p>Some text in the modal.</p>
